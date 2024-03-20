95c41b38126dd047614f64058d4f13a54d7e801c
ngenohkevin <ngenohkevin19@gmail.com>
2024-03-20 13:55:31 +0300

    migrations

---
+++ db/migration/000001_init_schema.down.sql
@@ -0,0 +1,13 @@
+
+drop table if exists property_owner cascade;
+
+drop table if exists property_agent cascade;
+
+drop table if exists property cascade;
+
+drop table if exists agent cascade;
+
+drop table if exists owner cascade;
+
+drop table if exists users cascade;
+
