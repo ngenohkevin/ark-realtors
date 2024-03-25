2464eeaa2455bf529f05c19d9120647578526cbd
ngenohkevin <ngenohkevin19@gmail.com>
2024-03-25 12:09:50 +0300

    implemented create User

---
+++ db/sqlc/error.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"errors"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+const (
+	ForeignKeyViolation = "foreign_key_violation"
+	UniqueViolation     = "unique_violation"
+)
+
+var ErrRecordNotFound = pgx.ErrNoRows
+
+var ErrUniqueViolation = &pgconn.PgError{
+	Code: UniqueViolation,
+}
+
+func ErrorCode(err error) string {
+	var pgErr *pgconn.PgError
+	if errors.As(err, &pgErr) {
+		return pgErr.Code
+	}
+	return ""
+}
