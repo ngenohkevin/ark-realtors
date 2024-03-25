86475b5ce080a008032b3cd1debd36aba9c8ac25
ngenohkevin <ngenohkevin19@gmail.com>
2024-03-25 12:09:52 +0300

    implemented create User

---
+++ cmd/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/ngenohkevin/ark-realtors/api"
+	"github.com/ngenohkevin/ark-realtors/internal/store"
+	"github.com/ngenohkevin/ark-realtors/pkg/utils"
+	"log"
+)
+
+func main() {
+	config, err := utils.LoadConfig(".")
+	if err != nil {
+		log.Fatalf("Error loading config: %v", err)
+	}
+
+	connPool, err := pgxpool.New(context.Background(), config.DbUrl)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	stores := store.NewStore(connPool)
+
+	// Start the server
+	server, err := api.NewServer(config, &stores)
+	if err != nil {
+		log.Fatalf("cannot create server: %v", err)
+	}
+	err = server.Start(config.ServerAddr)
+	if err != nil {
+		log.Fatalf("cannot start server: %v", err)
+	}
+}
