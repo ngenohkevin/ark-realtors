2df6e9848f3c0ac31a865088d1111d52a0f050f2
ngenohkevin <ngenohkevin19@gmail.com>
2024-03-25 12:09:03 +0300

    config files

--- utils/config.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-type Config struct {
-	DbDriver   string `mapstructure:"DB_DRIVER"`
-	DbSource   string `mapstructure:"DB_SOURCE"`
-	ServerAddr string `mapstructure:"SERVER_ADDR"`
-}
