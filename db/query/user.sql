d001e15b64aff3a214aa2cf882ff3f766cea319a
ngenohkevin <ngenohkevin19@gmail.com>
2024-03-25 12:09:55 +0300

    implemented create User

---
+++ db/query/user.sql
@@ -0,0 +1,19 @@
+-- name: CreateUser :one
+INSERT INTO users (id, username, full_name, email, hashed_password)
+VALUES ($1, $2, $3, $4, $5) RETURNING *;
+
+-- name: GetUser :one
+SELECT * FROM users WHERE id = $1 LIMIT 1;
+
+-- name: GetUserByEmail :one
+SELECT * FROM users WHERE email = $1 LIMIT 1;
+
+-- name: UpdateUser :exec
+UPDATE users
+SET username = $2,
+    full_name = $3,
+    email = $4,
+    hashed_password = $5
+WHERE id = $1
+RETURNING *;
+
