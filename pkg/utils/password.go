db6c0dfcd991e036b21acf7c635ce7d1d5abbaa9
ngenohkevin <ngenohkevin19@gmail.com>
2024-03-26 14:20:25 +0300

    Hash password

--- pkg/utils/password.go
+++ pkg/utils/password.go
@@ -1,1 +1,20 @@
 package utils
+
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// HashPassword returns the bcrypt hash of the password
+func HashPassword(password string) (string, error) {
+	HashPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("error hashing password: %w", err)
+	}
+	return string(HashPassword), nil
+}
+
+// CheckPassword checks if the password is correct
+func CheckPassword(password string, hashedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
