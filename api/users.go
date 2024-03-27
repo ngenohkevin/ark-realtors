b707cace050bc5122184b8509bdf6ba1fdeaf77b
ngenohkevin <ngenohkevin19@gmail.com>
2024-03-27 14:50:51 +0300

    sqlc generated code

--- api/users.go
+++ api/users.go
@@ -1,9 +1,11 @@
 package api
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	db "github.com/ngenohkevin/ark-realtors/db/sqlc"
+	"github.com/ngenohkevin/ark-realtors/pkg/utils"
 	"net/http"
 	"time"
 )
@@ -15,7 +17,7 @@ type createUserRequest struct {
 	Email    string `json:"email" binding:"required,email"`
 }
 
-type createUserResponse struct {
+type userResponse struct {
 	ID                uuid.UUID `json:"id"`
 	Username          string    `json:"username"`
 	FullName          string    `json:"full_name"`
@@ -31,13 +33,18 @@ func (server *Server) createUser(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
+	hashedPassword, err := utils.HashPassword(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
 
 	arg := db.CreateUserParams{
 		ID:             uuid.New(),
 		Username:       req.Username,
 		FullName:       req.FullName,
 		Email:          req.Email,
-		HashedPassword: req.Password,
+		HashedPassword: hashedPassword,
 	}
 
 	user, err := (*server.Store).CreateUser(ctx, arg)
@@ -50,7 +57,7 @@ func (server *Server) createUser(ctx *gin.Context) {
 		return
 	}
 
-	resp := createUserResponse{
+	resp := userResponse{
 		ID:                user.ID,
 		Username:          user.Username,
 		FullName:          user.FullName,
@@ -59,4 +66,42 @@ func (server *Server) createUser(ctx *gin.Context) {
 		CreatedAt:         user.CreatedAt,
 	}
 	ctx.JSON(http.StatusOK, resp)
+}
+
+type loginUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+}
+
+type loginUserResponse struct {
+	SessionID             string       `json:"session_id"`
+	AccessToken           string       `json:"access_token"`
+	AccessTokenExpiresAt  time.Time    `json:"access_token_expires_at"`
+	RefreshToken          string       `json:"refresh_token"`
+	RefreshTokenExpiresAt time.Time    `json:"refresh_token_expires_at"`
+	User                  userResponse `json:"user"`
+}
+
+func (server *Server) loginUser(ctx *gin.Context) {
+	var req loginUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	user, err := (*server.Store).GetUser(ctx, req.Username)
+	if err != nil {
+		if errors.Is(err, db.ErrRecordNotFound) {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+
+	err = utils.CheckPassword(req.Password, user.HashedPassword)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
 }
