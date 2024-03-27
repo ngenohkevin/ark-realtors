b82e2424dc004bb4b6981e4d0d26c7eadab463a5
ngenohkevin <ngenohkevin19@gmail.com>
2024-03-27 14:50:32 +0300

    sqlc query

--- db/query/user.sql
+++ db/query/user.sql
@@ -3,10 +3,7 @@ INSERT INTO users (id, username, full_name, email, hashed_password)
 VALUES ($1, $2, $3, $4, $5) RETURNING *;
 
 -- name: GetUser :one
-SELECT * FROM users WHERE id = $1 LIMIT 1;
-
--- name: GetUserByEmail :one
-SELECT * FROM users WHERE email = $1 LIMIT 1;
+SELECT * FROM users WHERE username = $1 LIMIT 1;
 
 -- name: UpdateUser :exec
 UPDATE users
