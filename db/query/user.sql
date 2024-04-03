baf14293c227e41dab4495538096ffce2f07bbdc
ngenohkevin <ngenohkevin19@gmail.com>
2024-04-03 14:32:27 +0300

    changed db design

--- db/query/user.sql
+++ db/query/user.sql
@@ -1,6 +1,6 @@
 -- name: CreateUser :one
-INSERT INTO users (id, username, full_name, email, hashed_password)
-VALUES ($1, $2, $3, $4, $5) RETURNING *;
+INSERT INTO users (id, username, full_name, email, hashed_password, role)
+VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;
 
 -- name: GetUser :one
 SELECT * FROM users WHERE username = $1 LIMIT 1;
@@ -10,7 +10,8 @@ UPDATE users
 SET username = $2,
     full_name = $3,
     email = $4,
-    hashed_password = $5
+    hashed_password = $5,
+    role = $6
 WHERE id = $1
 RETURNING *;
 
