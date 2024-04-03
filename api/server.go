86185e808b8e83b6a644a3d89bcf86f0af8117b8
ngenohkevin <ngenohkevin19@gmail.com>
2024-04-03 16:44:05 +0300

    login users

--- api/server.go
+++ api/server.go
@@ -1,23 +1,31 @@
 package api
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/ngenohkevin/ark-realtors/internal/handlers"
 	"github.com/ngenohkevin/ark-realtors/internal/store"
+	"github.com/ngenohkevin/ark-realtors/internal/token"
 	"github.com/ngenohkevin/ark-realtors/pkg/utils"
 )
 
 type Server struct {
-	Config utils.Config
-	Store  *store.Store
-	Router *gin.Engine
+	Config     utils.Config
+	Store      *store.Store
+	tokenMaker token.Maker
+	Router     *gin.Engine
 }
 
 func NewServer(config utils.Config, store *store.Store) (*Server, error) {
+	tokenMaker, err := token.NewJWTMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
 	server := &Server{
-		Config: config,
-		Store:  store,
-		Router: gin.Default(),
+		Config:     config,
+		Store:      store,
+		tokenMaker: tokenMaker,
+		Router:     gin.Default(),
 	}
 	server.SetUpRouter()
 	return server, nil
@@ -29,6 +37,7 @@ func (server *Server) SetUpRouter() {
 	router.GET("/albums", handlers.GetAlbums)
 
 	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
 }
 
 func (server *Server) Start(address string) error {
