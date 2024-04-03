8ea00da16dab8b0c7358871a57d55487340cd587
ngenohkevin <ngenohkevin19@gmail.com>
2024-04-03 14:32:14 +0300

    changed db design

--- db/migration/000001_init_schema.down.sql
+++ db/migration/000001_init_schema.down.sql
@@ -1,13 +1,3 @@
-
-drop table if exists property_owner cascade;
-
-drop table if exists property_agent cascade;
-
 drop table if exists property cascade;
-
-drop table if exists agent cascade;
-
-drop table if exists owner cascade;
-
 drop table if exists users cascade;
 
