280e320b591d79681affc94a0ab634c8766c38ba
ngenohkevin <ngenohkevin19@gmail.com>
2024-04-04 13:27:14 +0300

    remove role

--- db/query/user.sql
+++ db/query/user.sql
@@ -1,6 +1,6 @@
 -- name: CreateUser :one
-INSERT INTO users (id, username, full_name, email, hashed_password, role)
-VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;
+INSERT INTO users (id, username, full_name, email, hashed_password)
+VALUES ($1, $2, $3, $4, $5) RETURNING *;
 
 -- name: GetUser :one
 SELECT * FROM users WHERE username = $1 LIMIT 1;
