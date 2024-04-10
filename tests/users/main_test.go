177b6ff607cac5ba7ba8684f5e8087e680762bc6
ngenohkevin <ngenohkevin19@gmail.com>
2024-04-10 11:07:46 +0300

    fixed test

--- tests/users/main_test.go
+++ tests/users/main_test.go
@@ -1,21 +1,15 @@
 package users
 
 import (
-	"context"
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/ngenohkevin/ark-realtors/api"
 	"github.com/ngenohkevin/ark-realtors/internal/store"
 	"github.com/ngenohkevin/ark-realtors/pkg/utils"
 	"github.com/stretchr/testify/require"
-	"log"
 	"os"
 	"testing"
 	"time"
 )
-
-var testStore store.Store
 
 func newTestServer(t *testing.T, store store.Store) *api.Server {
 	config := utils.Config{
@@ -30,20 +24,6 @@ func newTestServer(t *testing.T, store store.Store) *api.Server {
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
-	cwd, _ := os.Getwd()
-	fmt.Println("current working directory: ", cwd)
-
-	config, err := utils.LoadConfig(".")
-	if err != nil {
-		log.Fatalf("cannot load config: %v", err)
-	}
-
-	connPool, err := pgxpool.New(context.Background(), config.DbUrl)
-	if err != nil {
-		log.Fatal("cannot connect to db:", err)
-	}
-	testStore = store.NewStore(connPool)
-
 	os.Exit(m.Run())
 
 }
