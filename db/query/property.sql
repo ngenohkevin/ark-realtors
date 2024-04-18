aeda917cda44073dff78380a8aebaf1a3f459c47
ngenohkevin <ngenohkevin19@gmail.com>
2024-04-18 13:37:33 +0300

    changed structure of the app

--- db/query/property.sql
+++ db/query/property.sql
@@ -1,6 +1,6 @@
 -- name: CreateProperty :one
-INSERT INTO property (id, type, price, status, pictures, bedroom, bathroom, location, size, contact, owner_id, agent_id)
-VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *;
+INSERT INTO property (id, type, price, status, img_url, bedroom, bathroom, location, size, contact)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *;
 
 -- name: GetProperty :one
 SELECT * FROM property WHERE id = $1 LIMIT 1;
@@ -15,7 +15,7 @@ UPDATE property
 SET type = $2,
     price = $3,
     status = $4,
-    pictures = $5,
+    img_url = $5,
     bedroom = $6,
     bathroom = $7,
     location = $8,
