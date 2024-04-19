c279c056033d9d2dd1a1a4ae6295f70b4f36531e
ngenohkevin <ngenohkevin19@gmail.com>
2024-04-19 12:07:53 +0300

    added pictures table

--- db/query/property.sql
+++ db/query/property.sql
@@ -1,6 +1,6 @@
 -- name: CreateProperty :one
-INSERT INTO property (id, type, price, status, img_url, bedroom, bathroom, location, size, contact)
-VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *;
+INSERT INTO property (id, type, price, status, bedroom, bathroom, location, size, contact)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *;
 
 -- name: GetProperty :one
 SELECT * FROM property WHERE id = $1 LIMIT 1;
@@ -15,12 +15,11 @@ UPDATE property
 SET type = $2,
     price = $3,
     status = $4,
-    img_url = $5,
-    bedroom = $6,
-    bathroom = $7,
-    location = $8,
-    size = $9,
-    contact = $10
+    bedroom = $5,
+    bathroom = $6,
+    location = $7,
+    size = $8,
+    contact = $9
 WHERE id = $1
 RETURNING *;
 
