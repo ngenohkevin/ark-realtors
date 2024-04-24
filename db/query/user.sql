c16dce55f55216c50f2e7f14156cca3c21f394a6
ngenohkevin <ngenohkevin19@gmail.com>
2024-04-24 13:20:10 +0300

    removed id when inserting users

--- db/query/user.sql
+++ db/query/user.sql
@@ -1,6 +1,6 @@
 -- name: CreateUser :one
-INSERT INTO users (id, username, full_name, email, hashed_password)
-VALUES ($1, $2, $3, $4, $5) RETURNING *;
+INSERT INTO users ( username, full_name, email, hashed_password)
+VALUES ($1, $2, $3, $4 ) RETURNING *;
 
 -- name: GetUser :one
 SELECT * FROM users WHERE username = $1 LIMIT 1;
