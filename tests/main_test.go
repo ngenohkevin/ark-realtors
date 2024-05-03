f4049bc5cbbef84ecbff5bd880eb5c89545e9102
ngenohkevin <ngenohkevin19@gmail.com>
2024-05-03 12:43:20 +0300

    loading of DB_URL

--- tests/main_test.go
+++ tests/main_test.go
@@ -2,7 +2,6 @@ package tests
 
 import (
 	"context"
-	"fmt"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/ngenohkevin/ark-realtors/internal/store"
 	"github.com/ngenohkevin/ark-realtors/pkg/utils"
@@ -16,14 +15,14 @@ var testStore store.Store
 func TestMain(m *testing.M) {
 
 	config1 := os.Getenv("DB_URL")
+	//if config1 == "" {
+	//	log.Fatal("environment variable is not set")
+	//}
 
 	config2, err := utils.LoadConfig("../.")
-	if err != nil && !os.IsNotExist(err) {
+	if err != nil {
 		log.Fatalf("cannot load config: %v", err)
 	}
-
-	fmt.Printf("config1: %v\n", config1)
-	fmt.Printf("config2: %v\n", config2)
 
 	var config string
 
@@ -32,9 +31,8 @@ func TestMain(m *testing.M) {
 	} else if config2.DbUrl != "" {
 		config = config2.DbUrl
 	} else {
-		log.Fatal("DB_URL environment variable and config file DB_URL are both empty")
+		log.Fatal("cannot load config")
 	}
-	fmt.Printf("config: %v\n", config)
 
 	connPool, err := pgxpool.New(context.Background(), config)
 	if err != nil {
