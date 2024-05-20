24f93f742302e8757f85565ca0bbea3a9b4d0c48
ngenohkevin <ngenohkevin19@gmail.com>
2024-05-20 20:52:02 +0300

    SQLC: update users

--- db/query/user.sql
+++ db/query/user.sql
@@ -5,13 +5,13 @@ VALUES ($1, $2, $3, $4, $5) RETURNING *;
 -- name: GetUser :one
 SELECT * FROM users WHERE username = $1 LIMIT 1;
 
--- name: UpdateUser :exec
+-- name: UpdateUser :one
 UPDATE users
-SET username = $2,
-    full_name = $3,
-    email = $4,
-    hashed_password = $5,
-    role = $6
-WHERE id = $1
+SET username = COALESCE(sqlc.narg(username), username),
+    full_name = COALESCE(sqlc.narg(full_name), full_name),
+    email = COALESCE(sqlc.narg(email), email),
+    hashed_password = COALESCE(sqlc.narg(hashed_password), hashed_password),
+    role = COALESCE(sqlc.narg(role), role)
+WHERE id = sqlc.arg(id)
 RETURNING *;
 
