cbbe0912ff6624e93adc78de4e77cfcc97b87681
ngenohkevin <ngenohkevin19@gmail.com>
2024-08-03 10:43:20 +0300

    update sessions

--- api/users.go
+++ api/users.go
@@ -255,6 +255,8 @@ func (server *Server) updateUser(ctx *gin.Context) {
 	}
 
 	// check if the user is the same as the one making the request
+	// Only an admin can add another user as an admin.
+	// users can only up
 
 	//authPayload := ctx.MustGet(AuthorizationPayloadKey).(*token.Payload)
 	//if authPayload.Role != utils.UserRole && authPayload.Username != user.Username {
