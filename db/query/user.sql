9889829bb7af4ecc38a5a5cbb719db7ca75d57e2
ngenohkevin <ngenohkevin19@gmail.com>
2024-08-20 11:13:27 +0300

    get user by id

--- db/query/user.sql
+++ db/query/user.sql
@@ -5,6 +5,9 @@ VALUES ($1, $2, $3, $4, $5) RETURNING *;
 -- name: GetUser :one
 SELECT * FROM users WHERE username = $1 LIMIT 1;
 
+-- name: GetUserById :one
+SELECT * FROM users WHERE id = $1 LIMIT 1;
+
 -- name: UpdateUser :one
 UPDATE users
 SET username = COALESCE(sqlc.narg(username), username),
