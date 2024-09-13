97c58fcb85e6b961a2c72ee80e59f7d985d9f973
ngenohkevin <ngenohkevin19@gmail.com>
2024-09-13 11:22:38 +0300

    renew access token route

--- api/server.go
+++ api/server.go
@@ -39,7 +39,7 @@ func (server *Server) SetUpRouter() {
 	//paths
 	router.POST("/users", server.createUser)
 	router.POST("/users/login", server.loginUser)
-	router.POST()
+	router.POST("/")
 
 	authRoutes := router.Group("/").Use(AuthMiddleware(server.TokenMaker))
 
